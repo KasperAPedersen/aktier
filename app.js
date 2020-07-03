04513f4b742beefbbacb17e50f3df9e198f7fad0
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2020-07-03 16:27:22 +0200

    Added stock

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let stocks = [
     "https://www.nordnet.dk/markedet/aktiekurser/16270886-iss",
     "https://www.nordnet.dk/markedet/aktiekurser/16122008-marriott-international-class",
     "https://www.nordnet.dk/markedet/aktiekurser/16123286-marriott-vacations-worldwide",
-    "https://www.nordnet.dk/markedet/aktiekurser/16118720-gilead-sciences"
+    "https://www.nordnet.dk/markedet/aktiekurser/16118720-gilead-sciences",
+    "https://www.nordnet.dk/markedet/aktiekurser/16214690-gilead-sciences"
 ];
 // --
 
