feab4c669536eedadd4e1bba037466164cd56b22
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2020-07-03 15:30:18 +0200

    Added stocks

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ let stocks = [
     "https://www.nordnet.dk/markedet/aktiekurser/16100078-sas",
     "https://www.nordnet.dk/markedet/aktiekurser/17258080-fom-technologies",
     "https://www.nordnet.dk/markedet/aktiekurser/16270886-iss",
-    "https://www.nordnet.dk/markedet/aktiekurser/16122008-marriott-international-class"
+    "https://www.nordnet.dk/markedet/aktiekurser/16122008-marriott-international-class",
+    "https://www.nordnet.dk/markedet/aktiekurser/16123286-marriott-vacations-worldwide"
 ];
 // --
 
